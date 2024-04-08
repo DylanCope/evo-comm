2b1171aab43896c0af44a611007e8582e087faff
Dylan Cope <dylanr.cope@gmail.com>
2024-04-08 05:14:01 -0700

    added partial overlap plot

---
+++ figures/mappo_reward_curves_with_partial/code.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from reproducible_figures.plotting import set_plotting_style
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+matplotlib.use("pdf")
+
+
+def plot(data):
+    set_plotting_style()
+    sns.lineplot(
+        data=data,
+        x="total_env_steps",
+        y="mean_total_reward",
+        hue="overlap_mode",
+        errorbar="se",
+    )
+    plt.xlabel("Steps")
+    plt.ylabel("Mean total reward")
+    plt.legend(title="Overlap Mode")
+
+
+def reproduce_figure():
+    data = [
+        pd.read_csv(csv_path)
+        for csv_path in Path("figures/mappo_reward_curves_with_partial").glob(
+            "data_*.csv"
+        )
+    ]
+    plot(*data)
+    plt.savefig(
+        "figures/mappo_reward_curves_with_partial/mappo_reward_curves_with_partial.pdf",
+        bbox_inches="tight",
+        dpi=1000,
+    )
+
+
+if __name__ == "__main__":
+    reproduce_figure()
