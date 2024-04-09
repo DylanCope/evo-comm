f8114ea5150a49eba4030e894bf66f054cd35708
Dylan Cope <dylanr.cope@gmail.com>
2024-04-09 08:30:44 -0700

    plot and param adjustments

--- experiments/mappo_with_on_prey_reward.sh
+++ experiments/mappo_with_on_prey_reward.sh
@@ -1,7 +1,7 @@
 python train.py -cn mce_mappo --multirun \
     N_AGENT_SOUNDS=8 \
     TOTAL_TIMESTEPS=5000000 \
-    ON_PREY_REWARD=0.1 \
+    PREY_AUDIBLE_RANGE=2 \
     N_PREY_SOUNDS=1 \
     SEED=0,1,2,3,4,5,6,7,8,9 \
     N_OVERLAPPING_SOUNDS=0,1
